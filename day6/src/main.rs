3390bdbf5205939dc614a043005b5551c4b75b5b
Matthew Else <matthewelse1997@gmail.com>
2017-12-06 14:39:15 +0000

    Day 6

---
+++ day6/src/main.rs
@@ -0,0 +1,80 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn argmax(vector: &Vec<usize>) -> (usize, usize) {
+    let mut current_max = vector[0];
+    let mut current_ix = 0;
+
+    for i in 0..vector.len() {
+        if vector[i] > current_max {
+            current_max = vector[i];
+            current_ix = i;
+        }
+    }
+
+    (current_ix, current_max)
+}
+
+/**
+ * It'd be nice to speed this up using a priority queue, but for the time-being,
+ * this should be enough to solve the problem pretty quickly.
+ *
+ * Using a binary queue would probably involve learning how to use Box<T>, so
+ * that could be fun.
+ */
+fn step(banks: &Vec<usize>) -> Vec<usize> {
+    let mut out = banks.to_vec();
+    let (ix, max) = argmax(banks);
+    out[ix] = 0;
+
+    for i in 0..banks.len() {
+        let j = (i + ix + 1) % banks.len();
+        let gain = max / banks.len();
+
+        if i < max % banks.len() {
+            out[j] += gain + 1;
+        } else {
+            out[j] += gain;
+        }
+    }
+
+    // println!("{:?} -> {:?}", banks, out);
+    out
+}
+
+fn part1(start: Vec<usize>) -> (i32, i32) {
+    let mut visited = HashMap::new();
+    let mut next = start.to_vec();
+    let mut steps = 0;
+
+    while !visited.contains_key(&next) {
+        visited.insert(next.to_vec(), steps);
+        next = step(&next);
+        steps += 1;
+    }
+
+    let previous = visited
+        .get(&next)
+        .expect("Unable to find pre-existing state");
+
+    (steps, steps - *previous)
+}
+
+fn main() {
+    println!("Example: {:?}", part1(vec![0, 2, 7, 0]));
+
+    // Load sample data for day6
+    let input = "day6.in";
+    let mut file = File::open(input).expect("Unable to open file day6.in");
+    let mut data = String::new();
+
+    let _ = file.read_to_string(&mut data);
+
+    let start: Vec<usize> = data.split(' ')
+        .filter(|x| x.len() > 0)
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+
+    println!("Both Parts: {:?}", part1(start));
+}
